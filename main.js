63f913872ae4a262511b2a4efd6213113e25652d
Kreoveta <backs.kaushil11@gmail.com>
2024-12-26 12:30:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// const shareData = {
+//     title: "MDN",
+//     text: "Learn web development on MDN!",
+//     url: "https://developer.mozilla.org",
+//   };
+  
+ 
+//   const resultPara = document.querySelector(".result");
+  
+//   // Share must be triggered by "user activation"
+//   btn.addEventListener("click", async () => {
+//     try {
+//       await navigator.share(shareData);
+//       resultPara.textContent = "MDN shared successfully";
+//     } catch (err) {
+//       resultPara.textContent = `Error: ${err}`;
+//     }
+//   });
+  
+const btn = document.getElementById("btn");
+
+  btn.addEventListener("click", async () => {
+    try {
+      await navigator.share({ title: "JCreation", url: "https://jcreation.kodocan.com/" });
+      console.log("Data was shared successfully");
+    } catch (err) {
+      console.error("Share failed:", err.message);
+    }
+  });
